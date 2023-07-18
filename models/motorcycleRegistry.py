85558f0fbf4319c3cda367574347b272acba74dd
rsma-odoo <rsma@odoo.com>
2023-07-18 17:51:00 -0600

    Corrected models format

--- models/motorcycleRegistry.py
+++ models/motorcycleRegistry.py
@@ -1,5 +1,27 @@
-from odoo import models
+from odoo import models,fields
 
 class MotorcycleRegistry(models.Model):
-    _name='motorcycle.registry'
-    _order = 'registry_number char required, vin char required, first_name char required, last_name char required, picture image, current_mileage float, license_plate char, certificate_title binary, register_date date '
+    _name = 'motorcycle.registry'
+    _description = 'Motorcycle Registry'
+    registry_number = fields.Char()
+    vin = fields.Char(string='', required=True)
+    first_name = fields.Char(string='',required=True)
+    last_name = fields.Char(string='',required=True)
+    picture = fields.Image(string='')
+    current_mileage = fields.Float(string='')
+    license_plate = fields.Char(string='')
+    certificate_title = fields.Binary(string='')
+    register_date = fields.Date(string='')
+
+
+"""registry_number - char, required, this field should be the name of the record.
+Hint: use the _rec_nameattribute in the Model to define this.
+vin - char, required
+first_name - char, required
+last_name - char, required
+picture - image
+current_mileage - float
+license_plate - char
+certificate_title - binary
+register_date - date
+"""
